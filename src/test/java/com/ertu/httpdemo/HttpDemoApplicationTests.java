6dcf3104eef3793e14f130e1fce831c29f01e5b3
Gump-Feng <houxfeng@outlook.com>
2019-09-24 10:47:35 +0800

    删整

--- src/test/java/com/ertu/httpdemo/HttpDemoApplicationTests.java
+++ src/test/java/com/ertu/httpdemo/HttpDemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.ertu.httpdemo;
 
 import com.ertu.httpdemo.pojo.User;
+import com.ertu.spider.utils.StringUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,10 @@ public class HttpDemoApplicationTests {
 		System.out.println(user);
 	}
 
+	@Test
+	public void regTest() {
+		String s = "jQuery17206571463072632691_1568901836495({resultCode:\"0000\",redirectURL:\"null\"})";
+		String resultByReg = StringUtil.getResultByReg(s, "resultCode:\"(.*?)\"");
+		System.out.println(resultByReg);
+	}
 }
