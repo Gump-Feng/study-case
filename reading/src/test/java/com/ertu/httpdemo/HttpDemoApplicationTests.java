f26e522b9fd64877f9f7dcf0bb757cd49dbe211e
houxfeng <houxfeng@outlook.com>
2021-07-01 10:31:18 +0800

    更新、

--- reading/src/test/java/com/ertu/httpdemo/HttpDemoApplicationTests.java
+++ reading/src/test/java/com/ertu/httpdemo/HttpDemoApplicationTests.java
@@ -1,12 +1,15 @@
 package com.ertu.httpdemo;
 
 import org.junit.Test;
+import redis.clients.jedis.Jedis;
 
 public class HttpDemoApplicationTests {
 
 
     @Test
     public void httpGet() {
+        Jedis jedis = new Jedis();
+
         System.out.println(Integer.MAX_VALUE);
     }
 
